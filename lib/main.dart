90c6671d4e9f02e1d54caaba8d81e9e254f64e02
gui1andre <gui1andre89@gmail.com>
2022-05-27 18:00:46 -0300

    preparação tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:crud/screens/add_pessoa.dart';
 import 'package:crud/screens/atualizarcadastro.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'components/textformfield.dart';
 import 'screens/dashboard.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class CrudApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       getPages: [
-        GetPage(name: '/', page: () => const Login()),
+        GetPage(name: '/', page: () => Login()),
         GetPage(name: '/dashboard', page: () => const Dashboard()),
         GetPage(name: '/cadastrarPessoa', page: () => const CadastrarPessoa()),
         GetPage(name: '/atualizarPessoa', page: () => const AtualizarPessoa()),
@@ -27,49 +28,61 @@ class CrudApp extends StatelessWidget {
 }
 
 class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
+  Login({Key? key}) : super(key: key);
+  final TextEditingController loginController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        top: true,
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Color.fromARGB(999, 59, 149, 254),
-                Colors.indigo,
-
-              ],
-            ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color.fromARGB(999, 59, 149, 254),
+              Colors.indigo,
+            ],
           ),
-          child: Center(
+        ),
+        child: SafeArea(
+          top: true,
+          child: SingleChildScrollView(
             child: Column(
               children: [
-                Container(
-                  width: 118,
-                  height: 118,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                  ),
-                  child: const Icon(
-                    Icons.person,
-                    color: Color.fromARGB(999, 59, 149, 254),
-                    size: 90,
+                Padding(
+                  padding: const EdgeInsets.only(top: 150),
+                  child: Container(
+                    width: 118,
+                    height: 118,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: const Icon(
+                      Icons.person,
+                      color: Color.fromARGB(999, 59, 149, 254),
+                      size: 90,
+                    ),
                   ),
                 ),
-                Form(child: Column(
-                  children: [TextFormField(),
-                  TextFormField(),
-                  ],
-                ),
-
+                Form(
+                  child: Column(
+                    children: [
+                      TextFieldForm(controler: loginController, textoCampo: 'Preencher o Login', nomeCampo: 'Login',),
+                      TextFieldForm(controler: senhaController, textoCampo: 'Preencher a Senha', nomeCampo: 'Senha',), 
+                      ElevatedButton(
+                          onPressed: () {
+                            Get.offNamed('dashboard');
+                          },
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blue.shade900),
+                          child: const Text('Efetuar Login'))
+                    ],
+                  ),
                 ),
               ],
             ),
